1d75860e457bcb67562e0ca3baf834cbf544a2b6
Vladislav Kutsenko <109062318+blazee5@users.noreply.github.com>
2023-02-10 18:46:02 +0600

    ЛПР 14 Зад. 5 Упр. 3

--- catalog.php
+++ catalog.php
@@ -1,39 +1,40 @@
 <?php
-	// подключение библиотек
-	require "inc/lib.inc.php";
-	require "inc/config.inc.php";
-    
-    $goods = selectAllItems();
+// подключение библиотек
+require "inc/lib.inc.php";
+require "inc/config.inc.php";
+global $count;
+$goods = selectAllItems();
 ?>
 <!DOCTYPE html>
 <html>
 <head>
-	<meta charset="utf-8">
-	<title>Каталог товаров</title>
+  <meta charset="utf-8">
+  <title>Каталог товаров</title>
 </head>
 <body>
-<p>Товаров в <a href="basket.php">корзине</a>: <?= $count?></p>
+<p>Товаров в <a href="basket.php">корзине</a>: <?= $count ?></p>
 <table border="1" cellpadding="5" cellspacing="0" width="100%">
-<tr>
-	<th>Название</th>
-	<th>Автор</th>
-	<th>Год издания</th>
-	<th>Цена, руб.</th>
-	<th>В корзину</th>
-</tr>
-<?php
-foreach ($goods as $item) {
+  <tr>
+    <th>Название</th>
+    <th>Автор</th>
+    <th>Год издания</th>
+    <th>Цена, руб.</th>
+    <th>В корзину</th>
+  </tr>
+  <?php
+  foreach ($goods as $item) {
     ?>
     <tr>
-        <td><?= $item['title']?></td>
-        <td><?= $item['author']?></td>
-        <td><?= $item['pubyear']?></td>
-        <? $id = $item['id']; ?>
-        <td><?= $item['price']?></td><td><a href="add2basket.php?<?=$id?>">В корзину</a></td>
+      <td><?= $item['title'] ?></td>
+      <td><?= $item['author'] ?></td>
+      <td><?= $item['pubyear'] ?></td>
+      <?php $id = $item['id']; ?>
+      <td><?= $item['price'] ?></td>
+      <td><a href="add2basket.php?id=<?= $id ?>">В корзину</a></td>
     </tr>
     <?php
-}
-?>
+  }
+  ?>
 </table>
 </body>
 </html>
--- inc/lib.inc.php
+++ inc/lib.inc.php
@@ -1,50 +1,50 @@
 <?php
 require "config.inc.php";
 function addItemToCatalog($title, $author, $pubyear, $price) {
-    $sql = 'INSERT INTO catalog (title, author, pubyear, price) VALUES (?, ?, ?, ?)';
-    global $link;
-    if (!$stmt = mysqli_prepare($link, $sql)) {
-        return false;
-    } else {
-        mysqli_stmt_bind_param($stmt, "ssii", $title, $author, $pubyear, $price);
-        mysqli_stmt_execute($stmt);
-        mysqli_stmt_close($stmt);
-        return true;
-    }
+  $sql = 'INSERT INTO catalog (title, author, pubyear, price) VALUES (?, ?, ?, ?)';
+  global $link;
+  if (!$stmt = mysqli_prepare($link, $sql)) {
+    return false;
+  } else {
+    mysqli_stmt_bind_param($stmt, "ssii", $title, $author, $pubyear, $price);
+    mysqli_stmt_execute($stmt);
+    mysqli_stmt_close($stmt);
+    return true;
+  }
 }
 
 function selectAllItems() {
-    global $link;
-    $sql = 'SELECT id, title, author, pubyear, price FROM catalog';
+  global $link;
+  $sql = 'SELECT id, title, author, pubyear, price FROM catalog';
 
-    if (!$result = mysqli_query($link, $sql)) {
-        return false;
-    } else {
-        $items = mysqli_fetch_all($result, MYSQLI_ASSOC);
-        mysqli_free_result($result);
-        return $items;
-    }
+  if (!$result = mysqli_query($link, $sql)) {
+    return false;
+  } else {
+    $items = mysqli_fetch_all($result, MYSQLI_ASSOC);
+    mysqli_free_result($result);
+    return $items;
+  }
 }
 
 function saveBasket() {
-    global $basket;
-    $basket = base64_encode(serialize($basket));
-    setcookie('basket', $basket, 0x7FFFFFFF);
+  global $basket;
+  $basket = base64_encode(serialize($basket));
+  setcookie('basket', $basket, 0x7FFFFFFF);
 }
 
 function basketInit() {
-    global $basket, $count;
-    if (!isset($_COOKIE['basket'])) {
-        $basket = ['orderid' => uniqid()];
-        saveBasket();
-    } else {
-        $basket = unserialize(base64_decode($_COOKIE['basket']));
-        $count = count($basket) - 1;
-    }
+  global $basket, $count;
+  if (!isset($_COOKIE['basket'])) {
+    $basket = ['orderid' => uniqid()];
+    saveBasket();
+  } else {
+    $basket = unserialize(base64_decode($_COOKIE['basket']));
+    $count = count($basket) - 1;
+  }
 }
 
 function add2Basket($id) {
-    global $basket;
-    $basket[$id] = 1;
-    saveBasket();
+  global $basket;
+  $basket[$id] = 1;
+  saveBasket();
 }
