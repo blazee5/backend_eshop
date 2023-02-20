13308105cde173668e36beaac968ce5a8be57503
blazee5 <vladislavkutsenko742@gmail.com>
2023-02-20 19:58:23 +0600

    ЛПР 15-16 Зад. 9 Упр. 1

--- inc/lib.inc.php
+++ inc/lib.inc.php
@@ -102,3 +102,40 @@ function saveOrder($datetime) {
     setcookie("basket", "", time()-3600);
     return true;
 }
+
+function getOrders() {
+    global $link;
+    if(!is_file(ORDERS_LOG))
+        return false;
+    /* Получаем в виде массива персональные данные пользователей из файла */
+    $orders = file(ORDERS_LOG);
+    /* Массив, который будет возвращен функцией */
+    $allorders = [];
+    foreach ($orders as $order) {
+        list($name, $email, $phone, $address, $orderid, $date) = explode("|",
+            $order);
+        /* Промежуточный массив для хранения информации о конкретном заказе */
+        $orderinfo = [];
+        /* Сохранение информацию о конкретном пользователе */
+        $orderinfo["name"] = $name;
+        $orderinfo["email"] = $email;
+        $orderinfo["phone"] = $phone;
+        $orderinfo["address"] = $address;
+        $orderinfo["orderid"] = $orderid;
+        $orderinfo["date"] = $date;
+        /* SQL-запрос на выборку из таблицы orders всех товаров для конкретного покупателя */
+        $sql = "SELECT title, author, pubyear, price, quantity
+        FROM orders
+        WHERE orderid = '$orderid' AND datetime = $date";
+        /* Получение результата выборки */
+        if(!$result = mysqli_query($link, $sql))
+            return false;
+        $items = mysqli_fetch_all($result, MYSQLI_ASSOC);
+        mysqli_free_result($result);
+        /* Сохранение результата в промежуточном массиве */
+        $orderinfo["goods"] = $items;
+        /* Добавление промежуточного массива в возвращаемый массив */
+        $allorders[] = $orderinfo;
+    }
+    return $allorders;
+}
